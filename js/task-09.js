4ee48fd294c4d0289cde88d8fdef2ce86353a047
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2023-09-12 13:22:46 +0300

    update#3

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector("button.change-color");
+const body = document.body;
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
